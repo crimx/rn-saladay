d0d908e5a47937e9f4d527b101c1bd90731e3026
CRIMX <straybugs@gmail.com>
2017-09-21 20:25:28 +0800

    Updated prefer-promise-reject-errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   // plugins: ['react'],
   // add your custom rules here
   'rules': {
+    'prefer-promise-reject-errors': 0,
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
