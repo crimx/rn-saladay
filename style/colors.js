4ed77f701731ec3dbaf0349e7237c455b6ed16c7
CRIMX <straybugs@gmail.com>
2017-09-25 02:18:30 +0800

    Add colors

--- style/colors.js
+++ style/colors.js
@@ -19,5 +19,98 @@ export default {
   primary: '#5caf9e',
   primaryLight: '#c2ebe3',
   primaryLightDark: '#8cd0c3',
-  primaryDark: '#3a9482'
+  primaryDark: '#3a9482',
+
+  grey: '#e7e9e9',
+  greyLight: '#f2f4f5',
+  greyDark: '#666666',
+
+  flatUiColors: [
+    '#ec644b', // SOFT RED
+    '#d24d57', // CHESTNUT ROSE
+    '#f22613', // POMEGRANATE
+    '#d91e18', // THUNDERBIRD
+    '#96281b', // OLD BRICK
+    '#ef4836', // FLAMINGO
+    '#d64541', // VALENCIA
+    '#c0392b', // TALL POPPY
+    '#cf000f', // MONZA
+    '#e74c3c', // CINNABAR
+    '#db0a5b', // RAZZMATAZZ
+    '#f64747', // SUNSET ORANGE
+    '#f1a9a0', // WAX FLOWER
+    '#d2527f', // CABARET
+    '#e08283', // NEW YORK PINK
+    '#f62459', // RADICAL RED
+    '#e26a6a', // SUNGLO
+    '#663399', // REBECCAPURPLE
+    '#674172', // HONEY FLOWER
+    '#913d88', // PLUM
+    '#9a12b3', // SEANCE
+    '#bf55ec', // MEDIUM PURPLE
+    '#be90d4', // LIGHT WISTERIA
+    '#8e44ad', // STUDIO
+    '#9b59b6', // WISTERIA
+    '#446cb3', // SAN MARINO
+    '#4183d7', // ROYAL BLUE
+    '#59abe3', // PICTON BLUE
+    '#81cfe0', // SPRAY
+    '#52b3d9', // SHAKESPEARE
+    '#22a7f0', // PICTON BLUE
+    '#3498db', // CURIOUS BLUE
+    '#2c3e50', // MADISON
+    '#19b5fe', // DODGER BLUE
+    '#336e7b', // MING
+    '#22313f', // EBONY CLAY
+    '#1e8bc3', // SUMMER SKY
+    '#3a539b', // CHAMBRAY
+    '#34495e', // PICKLED BLUEWOOD
+    '#67809f', // HOKI
+    '#2574a9', // JELLY BEAN
+    '#1f3a93', // JACKSONS PURPLE
+    '#4b77be', // STEEL BLUE
+    '#5c97bf', // FOUNTAIN BLUE
+    '#4ecdc4', // MEDIUM TURQUOISE
+    '#87d37c', // GOSSIP
+    '#90c695', // DARK SEA GREEN
+    '#26a65b', // EUCALYPTUS
+    '#03c9a9', // CARIBBEAN GREEN
+    '#68c3a3', // SILVER TREE
+    '#65c6bb', // DOWNY
+    '#1bbc9b', // MOUNTAIN MEADOW
+    '#1ba39c', // LIGHT SEA GREEN
+    '#66cc99', // MEDIUM AQUAMARINE
+    '#36d7b7', // TURQUOISE
+    '#2ecc71', // SHAMROCK
+    '#16a085', // NIAGARA
+    '#3fc380', // EMERALD
+    '#019875', // GREEN HAZE
+    '#03a678', // FREE SPEECH AQUAMARINE
+    '#4daf7c', // OCEAN GREEN
+    '#2abb9b', // NIAGARA 1
+    '#00b16a', // JADE
+    '#1e824c', // SALEM
+    '#049372', // OBSERVATORY
+    '#26c281', // JUNGLE GREEN
+    '#e9d460', // CONFETTI"
+    '#f89406', // CALIFORNIA
+    '#eb9532', // FIRE BUSH
+    '#e87e04', // TAHITI GOLD
+    '#f4b350', // CASABLANCA
+    '#f2784b', // CRUSTA
+    '#eb974e', // SEA BUCKTHORN
+    '#f5ab35', // LIGHTNING YELLOW
+    '#d35400', // BURNT ORANGE
+    '#f39c12', // BUTTERCUP
+    '#f9690e', // ECSTASY
+    '#f9bf3b', // SANDSTORM
+    '#f27935', // JAFFA
+    '#e67e22', // ZEST
+    '#6c7a89', // LYNCH
+    '#d2d7d3', // PUMICE
+    '#bdc3c7', // SILVER SAND
+    '#95a5a6', // CASCADE
+    '#abb7b7', // EDWARD
+    '#bfbfbf' // SILVER
+  ]
 }
